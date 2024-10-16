bf4c5d3058e5ee6e4db444fbcdb3ca9036b0c784
shenjiachuan <153788451@qq.com>
2024-10-16 16:46:44 +0800

    打印类的源代码

--- src/main/java/com/sjc/javaAgent/command/ClassCommand.java
+++ src/main/java/com/sjc/javaAgent/command/ClassCommand.java
@@ -1,9 +1,19 @@
 package com.sjc.javaAgent.command;
 
+import org.jd.core.v1.ClassFileToJavaSourceDecompiler;
+import org.jd.core.v1.api.loader.Loader;
+import org.jd.core.v1.api.loader.LoaderException;
+import org.jd.core.v1.api.printer.Printer;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.lang.instrument.UnmodifiableClassException;
+import java.security.ProtectionDomain;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 /**
@@ -35,4 +45,96 @@ public class ClassCommand {
          //去重过滤掉相同的反射优化的类加载器
          System.out.println(collect);
      }
+
+    //打印类的源代码
+    public static void printClassSourceCode (Instrumentation inst) {
+        System.out.println("请输入类名: ");
+        //输入类名
+        Scanner scanner = new Scanner(System.in);
+        String className = scanner.next();
+
+        //根据类名找到class对象
+        Class[] allLoadedClasses = inst.getAllLoadedClasses();
+        for (Class allLoadedClass : allLoadedClasses) {
+            if (allLoadedClass.getName().equals(className)) {
+                //添加转换器
+                ClassFileTransformer classFileTransformer = new ClassFileTransformer() {
+                    // 类的转换器本来可以增强类信息，这边是起获取到类的字节码信息的功能
+                    @Override
+                    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+                        //通过jd-core反编译并打印源代码
+                        try {
+                            printJDCoreSourceCode(classfileBuffer , className);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        return ClassFileTransformer.super.transform(loader, className, classBeingRedefined, protectionDomain, classfileBuffer);
+                    }
+                };
+                inst.addTransformer(classFileTransformer , true);
+
+                // 触发转换器
+                try {
+                    inst.retransformClasses(allLoadedClass);
+                } catch (UnmodifiableClassException e) {
+                    throw new RuntimeException(e);
+                }finally {
+                    //删除转换器
+                    inst.removeTransformer(classFileTransformer);
+                }
+            }
+        }
+    }
+    //通过jd-core打印源代码
+    private static void printJDCoreSourceCode (byte[] bytes , String className) throws Exception {
+         //load对象
+        Loader loader = new Loader() {
+            @Override
+            public boolean canLoad(String s) {
+                return true;
+            }
+
+            @Override
+            public byte[] load(String s) throws LoaderException {
+                return bytes;
+            }
+        };
+        Printer printer = new Printer() {
+            protected static final String TAB = "  ";
+            protected static final String NEWLINE = "\n";
+
+            protected int indentationCount = 0;
+            protected StringBuilder sb = new StringBuilder();
+
+            @Override public String toString() { return sb.toString(); }
+
+            @Override public void start(int maxLineNumber, int majorVersion, int minorVersion) {}
+            @Override public void end() {
+                // 打印源代码
+                System.out.println(sb);
+            }
+
+            @Override public void printText(String text) { sb.append(text); }
+            @Override public void printNumericConstant(String constant) { sb.append(constant); }
+            @Override public void printStringConstant(String constant, String ownerInternalName) { sb.append(constant); }
+            @Override public void printKeyword(String keyword) { sb.append(keyword); }
+            @Override public void printDeclaration(int type, String internalTypeName, String name, String descriptor) { sb.append(name); }
+            @Override public void printReference(int type, String internalTypeName, String name, String descriptor, String ownerInternalName) { sb.append(name); }
+
+            @Override public void indent() { this.indentationCount++; }
+            @Override public void unindent() { this.indentationCount--; }
+
+            @Override public void startLine(int lineNumber) { for (int i=0; i<indentationCount; i++) sb.append(TAB); }
+            @Override public void endLine() { sb.append(NEWLINE); }
+            @Override public void extraLine(int count) { while (count-- > 0) sb.append(NEWLINE); }
+
+            @Override public void startMarker(int type) {}
+            @Override public void endMarker(int type) {}
+        };
+        //通过jd-core 方法打印
+        ClassFileToJavaSourceDecompiler decompiler = new ClassFileToJavaSourceDecompiler();
+
+        decompiler.decompile(loader, printer, className);
+
+    }
 }
